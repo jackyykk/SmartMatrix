a8a6dbda15345481b97d710836833b54eae688e7
DT1-MF\jacky <jackyykk@gmail.com>
2024-10-19 22:57:51 +0800

    Add SmartMatrix.Domain.csproj file

---
+++ src/SmartMatrix.Domain/Constants/CommonConstants.cs
@@ -0,0 +1,11 @@
+namespace SmartMatrix.Domain.Constants
+{
+    public static class CommonConstants
+    {
+        public static class Status
+        {
+            public const string Active = "Active";
+            public const string Inactive = "Inactive";
+        }
+    }
+}
